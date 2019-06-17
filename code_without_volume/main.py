940a1af7a372ab7b50ab8cb14bf3b52244308e11
jeff-maes <jeffmaes.jm@gmail.com>
2019-06-17 09:40:51 +0200

    code without volume

---
+++ code_without_volume/main.py
@@ -0,0 +1,59 @@
+'''AEX stock predication algorithm based on a LSTM
+        Authors:    Benjamin De Bosscher
+                    Jeff Maes
+                    Daan Cuppen
+                    Isaac Seminck
+                    Kipras Paliušis
+        Based on a tutorial of Thushan Ganegedara (https://www.datacamp.com/community/tutorials/lstm-python-stock-market)
+'''
+
+from src.data_operations.import_as_dict_backup import get_data
+from src.data_operations.preprocessing_backup import PreProc
+from src.LSTM_backup import LSTM
+from src.makeplots_backup import prediction
+from src.performance_output.txt_saver import PerformanceSaver
+
+# =============================================================================
+# Preprocessing
+# =============================================================================
+
+# Import data
+data_source = 'git'
+market = 'AEX'
+stocks = get_data(data_source, market)
+
+# ONLY FOR NOW, SHOULD BE CHANGED!!
+df = stocks['URW']
+
+# Preprocessing data
+split_datapoint = 5000         #5000 for PHIA
+smoothing_window_size = 1000    #1000 for PHIA
+
+pp_data = PreProc(df)
+pp_data.splitdata(split_datapoint)
+pp_data.normalize_smooth(smoothing_window_size, EMA=0.0, gamma=0.1)
+
+# =============================================================================
+# Define and apply LSTM
+# =============================================================================
+
+# Define hyperparameters
+D = 1                           # Dimensionality of the data. Since our data is 1-D this would be 1
+num_unrollings = 50             # Number of time steps you look into the future.
+batch_size = 500                # Number of samples in a batch
+num_nodes = [200, 200, 150]     # Number of hidden nodes in each layer of the deep LSTM stack we're using
+n_layers = len(num_nodes)       # number of layers
+dropout = 0.2                   # Dropout amount
+
+n_predict_once = 50
+
+# Run LSTM
+x_axis_seq, predictions_over_time = LSTM(pp_data, D, num_unrollings, batch_size, num_nodes, n_layers, dropout)
+
+# =============================================================================
+# Visualisation of the results
+# =============================================================================
+
+# Visualisation
+best_prediction_epoch = 22    # Replace this with the epoch that you got the best results when running the plotting code
+prediction(df, pp_data, x_axis_seq, predictions_over_time, best_prediction_epoch)
---
+++ code_without_volume/src/performance_output/txt_saver.py
@@ -0,0 +1,77 @@
+"""
+Created on Sun May 19 16:56:05 2019
+
+@author: Kiprot
+"""
+import datetime as dt
+import numpy as np
+import pandas as pd
+def PerformanceSaver(data_obj,  n_predict_once, num_unrollings, batch_size):
+	'''
+	This is used to return the lowest MSE run and a text file
+	with the relevant time sequence parameters:
+	1) train data sequence length (done by remove_data setting)
+	2) num_unrolling (how many time steps the model looks into)
+	3) batch_size (number of data samples at each time step)
+	4) n_predict_once (how many steps are predicted for in the future)
+	and the MSE per time step in a text file
+	to keep track of how changes to the time sequence length
+	affects the performance of the model
+	Save format: Run date, Best epoch, KPI that lead to the best epoch, Data Size, Num_unrolling, Batch size,
+	n_predict_once, KPI from that epoch
+
+	'''
+	SAVE_FOLDER = 'src/performance_output/PerformanceFiles/' # Folder to save the run results
+	title = SAVE_FOLDER + 'Ntot' + str(np.shape(data_obj.train_data)[0]) + '_Npred' + str(n_predict_once)+'.txt' # Title for text file
+	header = np.array(('Train data size =' + str(np.shape(data_obj.train_data)[0]),
+					   'Num_unrolling = ' + str(num_unrollings),
+					   'batch_size = ' + str(batch_size),
+					   'n_predict_once = ' + str(n_predict_once)))
+	header = np.reshape(header, (4,1))
+
+	###
+	headers = list(KPI.keys())
+	indices = [] # Index list for index of each lowest KPI from every epoch
+	for header in headers:
+		indices.append(KPI[header].index(min(KPI[header])))
+		ind = KPI[header].index(min(KPI[header])) # Index for minimum kpi
+#		print('KPI: ', header, ' INDEX ', ind, ' VALUES , ', KPI[header])
+	best_ind = max(set(indices), key = indices.count) # Taking the most frequent index within indices
+
+	# Outputting text file
+	np.savetxt(title, output_file, fmt = '%s')
+
+
+# =============================================================================
+# 				### Saving all data in CSV file
+# =============================================================================
+#	headers = list(KPI.keys())
+#	indices = [] # Index list for index of each lowest KPI from every epoch
+#			### Read current All_data csv file
+#	data_saver = pd.read_csv('src/performance_output/PerformanceFiles/All_results.csv')
+#	for header in headers:
+#		ind = KPI[header].index(min(KPI[header]))
+#		indices.append(ind)
+#	ind_chosen = max(set(indices))
+#	header2 = ['Date', 'Best epoch', 'From KPI:', 'Data size', 'Num_unrolling', 'Batch_size', 'n_predict_once'] +  headers
+#	data_temp = np.zeros(np.shape(header2)[0])
+#
+#	# Making the data row
+#	KPI_elems = ''
+##	data_obj = pp_data[0] ### REMOVE THIS LATER
+#	res_list = [i for i, value in enumerate(indices) if value == ind_chosen]
+#	for i in res_list:
+#		KPI_elems += headers[i] + ', '
+#	KPI_elems = KPI_elems[:-2]
+#	data_temp = {header2[0] : dt.datetime.now().ctime(), header2[1] : ind_chosen, header2[2] : KPI_elems, header2[3] : np.shape(data_obj.train_data)[0], header2[4] : num_unrollings, header2[5] : batch_size, header2[6] : n_predict_once}
+#	for h in headers:
+#		data_temp[h] = KPI[h][ind_chosen]
+#	data_toadd = pd.DataFrame(data_temp, index = [0])
+#	data_all = pd.concat([data_saver, data_toadd], ignore_index = True)
+##	data_all = pd.concat([data_saver, data_toadd], ignore_index = True)
+#	data_all.to_csv('src/performance_output/PerformanceFiles/All_results.csv', index = False)
+#	print('Best prediction epoch: ', ind_chosen, ' with KPI: ')
+#	for key in KPI.keys():
+#		print('KPI: ', key, ' =', KPI[key][ind_chosen])
+
+	return ind_chosen # Return best prediction epoch
